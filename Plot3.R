c1ffda61eb361c52316c40182d8cfe8648e8ee6a
jverboon <jeffverboon@gmail.com>
2015-04-12 16:10:56 -0700

    hope this works

---
+++ Plot3.R
@@ -0,0 +1,30 @@
+## Read in data, requires internet connection, may take awhile
+
+temp <- tempfile()
+download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+electric <- read.table(unz(temp, "household_power_consumption.txt"), 
+                       sep = ";", header = TRUE, stringsAsFactors = FALSE, na.strings = "?")
+unlink(temp)
+
+##subset to important dates
+
+electric_feb <- subset(electric, Date=="1/2/2007"|Date=="2/2/2007")
+
+##convert to date and time class objects
+electric_feb$datetime <- paste(electric_feb$Date, electric_feb$Time, sep = " ")
+electric_feb$datetime <- strptime(electric_feb$datetime, "%d/%m/%Y %H:%M:%S")
+
+##make file
+png(filename = "plot3.png",
+    width = 480, height = 480, units = "px")
+
+##plot3
+plot(electric_feb$datetime, electric_feb$Sub_metering_1, type = "l", 
+                        ylab = "Energy sub metering", xlab ="")
+with(electric_feb, lines(datetime, Sub_metering_2, col = "Red"))
+with(electric_feb, lines(datetime, Sub_metering_3, col = "Blue"))
+with(electric_feb, legend("topright", lwd = 1, col = c("black", "blue", "red"), 
+                          legend = c("Sub_metering_1", "Sub_metering_2", 
+                                     "Sub_metering_3")))
+dev.off()
+     
